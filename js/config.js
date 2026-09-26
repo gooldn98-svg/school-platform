9d6bb70b4d4e4acde1acc8a3bd73b78407c3c1fd
gooldn98-svg <gooldn98@gmail.com>
2026-09-26 23:59:49 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,7 @@
-// رابط المشروع ومفتاح الواجهة البرمجية
-const SUPABASE_URL = 'https://dszeximmfnsmcfdccofv.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_ng8OyJTQp4iCyuLkcwVe2Q_sH7x2VUV';
+// js/config.js
+
+const SUPABASE_URL = "https://dszeximmfnsmcfdccofv.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ng8OyJTQp4iCyuLkcwVe2Q_sH7x2VUV";
+
+// تهيئة عميل Supabase
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
